efa43d70973ea698ec0a339a4185992358d7a752
MrStanda <stanial19@sps-prosek.cz>
2022-09-09 15:02:25 +0200

    Create AboutView.swift

---
+++ IOSapp/Views/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  IOSapp
+//
+//  Created by Aleš Stanislav on 09.09.2022.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
